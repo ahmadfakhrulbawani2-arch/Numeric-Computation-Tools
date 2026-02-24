3d33e88c81f7d92a7c2523404e78b5364e24975b
Ahmad Fakhrul Bawani <ahmadfakhrulbawani2@gmail.com>
2026-02-25 06:44:27 +0700

    fix: Newton-Raphson done

--- src/Newton-Raphson/rootFindingProb.py
+++ src/Newton-Raphson/rootFindingProb.py
@@ -1,29 +1,109 @@
-from src.utils.FuncUtils import DerivativeF
-from src.utils.FuncUtils import CalcFunc
+import time
+from typing import List
+from src.utils.FuncUtils import CalcFunc, DerivativeF, Calc_Converngence_Rate
 
-# currently available only for power 3 for testing
-def main(nEq, nInit):
-  nEqLen = len(nEq)
-  nEq = list(reversed(nEq))
-  nFx0 = CalcFunc(nEq, nInit)
-  nFPrimex0 = 0
-  if nEqLen > 4:
-    print("Sorry currently can't handle beyond 2-nd power")
-    return
-  else:
-    arrDerivedEq = DerivativeF(nEq)
-    if arrDerivedEq == 0:
-      print("Unable to solve as the derivative of given input is 0")
-      return
-    
-    nFPrimex0 = CalcFunc(arrDerivedEq, nInit)
-    res = nInit - (nFx0 / nFPrimex0)
-  
+MAX_ITERATION: int = 50
+
+# This return Xn = Xn-1 - (f(Xn-1) / f'(Xn-1))
+def newton_raphson(eq: List[int], xInit: float) -> float | None:
+  new_eq: List[int] = list(reversed(eq))
+  fx0: float = CalcFunc(new_eq, xInit)
+  arrDerivedEq: List[int] = DerivativeF(new_eq)
+
+  if not arrDerivedEq:
+    print("Unable to solve as the derivative of given input is 0")
+    return None
+
+  fPrimex0: float = CalcFunc(arrDerivedEq, xInit)
+  if fPrimex0 == 0:
+    print("Derivative value is zero")
+    return None
+
+  res: float = xInit - (fx0 / fPrimex0)
   return res
 
+# This return equation string
+def get_eq_state(eq: List[int]) -> str:
+  new_eq: List[int] = list(reversed(eq))
+  sEq = ""
 
+  # reverse the loop to reverse the printed eq
+  for i in range(len(new_eq)-1, -1, -1): 
+    if new_eq[i]:
+      if i > 1:
+        sEq += f"({new_eq[i]})X^{i} + "
+      elif i == 1:
+        sEq += f"({new_eq[i]})X + "
+      else:
+        sEq += f"({new_eq[i]}) + "
+
+  return "Equation f(x) = " + sEq.rstrip(" + ")
+
+
+def printIter(res: float, iter: int, err_rate: float) -> None:
+  if err_rate is not None: 
+    print(f"Iteration-{iter} X{iter} = {res:.4f} | convergence rate ≈ {err_rate:.4f}")  
+  else: 
+    print(f"Iteration-{iter} X{iter} = {res:.4f}")
+  time.sleep(0.2)  # delay 0.2 second
+  return None
+
+# This return the root of input func
+def main(eq: List[int], x_init: float) -> float | None:
+  errors: List[float] = [] # store convergence rate every iterations
+  iter: int = 1
+  prev_x: float = x_init
+  curr_x: float | None = newton_raphson(eq, prev_x) # Because newton_raphson can return None
+
+  if curr_x is None:
+    return None
+
+  print("\n========= Newton-Raphson Finding Iteration =========\n")
+  print(f"Initial value of X0 is {x_init}\n")
+
+  new_eq = list(reversed(eq))
+
+  while abs(CalcFunc(new_eq, curr_x)) > 1e-6 and iter < MAX_ITERATION:
+    prev_x = curr_x
+    curr_x = newton_raphson(eq, prev_x)
+
+    if curr_x is None:
+      return None
+
+    errors.append(abs(curr_x - prev_x))
+    err_rate: float | None = Calc_Converngence_Rate(errors)
+    printIter(curr_x, iter, err_rate)
+    iter += 1
+
+  if iter == MAX_ITERATION:
+    print(f"Maximum number of iteration reached of {MAX_ITERATION} iterations!")
+    return None
+
+  return curr_x
+
+def print_sol(nRes: float | None, nEq: List[int]) -> None:
+  if nRes is None:
+    return
+  sEqState = get_eq_state(nEq)
+  print(f"\nThe root of {sEqState} from Newton-Raphson computation is x = {nRes}")
+
+# main driver
 if __name__ == "__main__":
-  arrEq = list(map(int, input("Masukkan koefisien (spasi): ").split()))
-  nInitVal = int(input("Masukkan nilai awal x0: "))
-  res = main(arrEq, nInitVal)
-  print(f"Hasil root finding menggunakan Newton-Raphson adalah = {res}")
+  while True:
+    user_input = input(
+      "\nInput function coeffissients (split with space, integer only) or 'q' to exit: "
+    ).strip()
+
+    if user_input.lower() == "q":
+      print("Exiting program...")
+      break
+
+    try:
+      arrEq = list(map(int, user_input.split()))
+      nInitVal = float(input("Input initial value of x (x0): "))
+
+      res = main(arrEq, nInitVal)
+      print_sol(res, arrEq)
+
+    except ValueError:
+      print("Invalid input. Please enter integers only or 'q' to exit.")
--- src/utils/FuncUtils.py
+++ src/utils/FuncUtils.py
@@ -1,11 +1,19 @@
-def DerivativeF(nEq):
-  nFPrime = []
-  for i in range(1, len(nEq)):
-    nFPrime.append(nEq[i] * i) 
-  return nFPrime
+from typing import List
+import math
 
-def CalcFunc(nEq, x):
-  nRes = 0
-  for i in range(len(nEq)):  
-    nRes += nEq[i] * (x ** i)
-  return nRes
+def DerivativeF(coeffs: List[float]) -> List[float]:
+  return [coeffs[i] * i for i in range(1, len(coeffs))]
+
+
+def CalcFunc(coeffs: List[float], x: float) -> float:
+  return sum(coeffs[i] * (x ** i) for i in range(len(coeffs)))
+
+def Calc_Converngence_Rate(err: List[float]) -> float | None:
+  if len(err) < 3: 
+    return None
+  
+  e_n1: float = err[-1] 
+  e_n: float = err[-2]
+  e_n_1: float = err[-3]
+
+  return math.log(e_n1 / e_n) / math.log(e_n / e_n_1)
